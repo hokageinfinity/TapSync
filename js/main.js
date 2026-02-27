5a948eace872e618578f8f31a7b31545719d9658
hokageinfinity <nateandjess1218@gmail.com>
2026-02-27 15:56:46 -0500

    Add event listeners for audio file loading

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+document.getElementById("loadBtn").addEventListener("click", () => {
+    document.getElementById("musicInput").click();
+});
 
+document.getElementById("musicInput").addEventListener("change", async function(e) {
+    if (!e.target.files[0]) return;
+
+    await loadAudioFile(e.target.files[0]);
+    generateTestBeatmap();
+    startSong();
+    requestAnimationFrame(gameLoop);
+});
