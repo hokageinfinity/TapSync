653c3646c3e4d99f45bca7b24f751c418849969a
hokageinfinity <nateandjess1218@gmail.com>
2026-02-27 16:30:22 -0500

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,18 @@
+export function saveSong(name, arrayBuffer) {
+    localStorage.setItem("song_" + name, btoa(
+        String.fromCharCode(...new Uint8Array(arrayBuffer))
+    ));
+}
 
+export function getSongs() {
+    return Object.keys(localStorage)
+        .filter(k => k.startsWith("song_"))
+        .map(k => k.replace("song_", ""));
+}
+
+export function loadSongData(name) {
+    const base64 = localStorage.getItem("song_" + name);
+    const binary = atob(base64);
+    const bytes = Uint8Array.from(binary, c => c.charCodeAt(0));
+    return bytes.buffer;
+}
