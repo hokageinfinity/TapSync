8caddd129d4f554a6832be5a07157521e678d5b1
hokageinfinity <nateandjess1218@gmail.com>
2026-02-27 16:33:36 -0500

    Update audioEngine.js

--- js/audioEngine.js
+++ js/audioEngine.js
@@ -1,21 +1,24 @@
 export let audioCtx;
-export let audioBuffer;
-let songStartTime = 0;
+let source;
+let startTime = 0;
 
-export async function loadSong(file) {
+export async function initAudio() {
     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    const arrayBuffer = await file.arrayBuffer();
-    audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+    await audioCtx.resume();
 }
 
-export function playSong() {
-    const source = audioCtx.createBufferSource();
-    source.buffer = audioBuffer;
+export async function loadFromBuffer(arrayBuffer) {
+    return await audioCtx.decodeAudioData(arrayBuffer);
+}
+
+export function play(buffer) {
+    source = audioCtx.createBufferSource();
+    source.buffer = buffer;
     source.connect(audioCtx.destination);
-    songStartTime = audioCtx.currentTime;
+    startTime = audioCtx.currentTime;
     source.start();
 }
 
-export function getSongTime() {
-    return audioCtx.currentTime - songStartTime;
+export function getTime() {
+    return audioCtx.currentTime - startTime;
 }
